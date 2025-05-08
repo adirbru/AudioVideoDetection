9dde22fe9b0a75799561f7f9a6a147049f877910
ShaiGold <shaigoldenberg4@gmail.com>
2025-05-08 17:01:43 +0300

    added maxs inidices for find peaks

--- audio/main.py
+++ audio/main.py
@@ -26,26 +26,38 @@ def plot(abs_samples, sample_rate, threshold=0, starts=[], ends=[]):
     plt.show()
 
 
+import numpy as np
+import numba as nb
+
 @nb.njit
 def find_peaks(abs_arr, threshold, hold_for_unify):
     starts, ends = [], []
     in_peak = False
     peak_start = 0
+    max_in_peak = 0
+    max_ind_in_peak = 0
+    max_indices = []
 
     for i, val in enumerate(abs_arr):
         if val >= threshold:
             if not in_peak:
                 peak_start = i
                 in_peak = True
+            if val >= max_in_peak:
+                max_in_peak = val
+                max_ind_in_peak = i
         else:
             if in_peak:
                 peak_end = i - 1
-                # Unify with previous peak if close enough
                 if starts and (peak_start - ends[-1]) <= hold_for_unify:
-                    ends[-1] = peak_end  # Extend previous peak
+                    ends[-1] = peak_end
+                    if max_in_peak > abs_arr[max_indices[-1]]:
+                        max_indices[-1] = max_ind_in_peak
                 else:
                     starts.append(peak_start)
                     ends.append(peak_end)
+                    max_indices.append(max_ind_in_peak)
+                max_in_peak = 0
                 in_peak = False
 
     # Handle case where array ends while in a peak
@@ -53,21 +65,34 @@ def find_peaks(abs_arr, threshold, hold_for_unify):
         peak_end = len(abs_arr) - 1
         if starts and (peak_start - ends[-1]) <= hold_for_unify:
             ends[-1] = peak_end
+            if max_in_peak > abs_arr[max_indices[-1]]:
+                max_indices[-1] = max_ind_in_peak
         else:
             starts.append(peak_start)
             ends.append(peak_end)
+            max_indices.append(max_ind_in_peak)
 
-    return np.array(starts), np.array(ends)
+    return np.array(starts), np.array(ends), np.array(max_indices)
 
-
-sound_path = r"C:\Users\USER\PycharmProjects\AudioVideoDetection\GalClaps.wav.wav"
+name = "gal_clap"
+id_to_wav = {
+    "gal_clap": r"C:\Users\USER\PycharmProjects\AudioVideoDetection\GalClaps.wav.wav",
+    "skate": r"C:\Users\USER\PycharmProjects\AudioVideoDetection\skateboard.wav",
+    "man_clap": r"C:\Users\USER\PycharmProjects\AudioVideoDetection\clap_with_sound.wav"
+}
+# sound_path = r"C:\Users\USER\PycharmProjects\AudioVideoDetection\GalClaps.wav.wav"
 # sound_path = r"C:\Users\USER\PycharmProjects\AudioVideoDetection\skateboard.wav"
-sound_path = r"C:\Users\USER\PycharmProjects\AudioVideoDetection\clap_with_sound.wav"
+# sound_path = r"C:\Users\USER\PycharmProjects\AudioVideoDetection\clap_with_sound.wav"
+sound_path = id_to_wav[name]
 skate = "skate" in sound_path
+
+
+
 fps = 29.96
 minimum_diff_frames = 1
 minimum_diff_s = minimum_diff_frames / fps
 # Load WAV file
+write = False
 if __name__ == '__main__':
     sound_sample_rate, data = wavfile.read(sound_path)
 
@@ -92,7 +117,7 @@ if __name__ == '__main__':
     valid_times = og_indices / sound_sample_rate
     print(f"{valid_times = }")
 
-    ind_starts, ind_ends = find_peaks(abs_samples, threshold=threshold, hold_for_unify=minimum_diff_s * sound_sample_rate)
+    ind_starts, ind_ends, _ = find_peaks(abs_samples, threshold=threshold, hold_for_unify=minimum_diff_s * sound_sample_rate)
     starts, ends = ind_starts / sound_sample_rate, ind_ends / sound_sample_rate
     for s, e in zip(starts, ends):
         print(s, e)
@@ -102,7 +127,9 @@ if __name__ == '__main__':
     formatted_times = [{"timestamp_ms": f"{time:.3f}"} for time in starts]
 
     # Export to JSON
-    name = "skate"
-    name = "man_clap"
-    with open(f"{name}_timestamps.json", "w") as f:
-        json.dump(formatted_times, f, indent=2)
+
+    if write:
+        with open(f"{name}_timestamps.json", "w") as f:
+            json.dump(formatted_times, f, indent=2)
+    # with open(f"{name}_timestamps.json", "w") as f:
+    #     json.dump(formatted_times, f, indent=2)
