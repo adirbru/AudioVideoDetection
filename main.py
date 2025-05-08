9b0c88c8579f02f8f59a05f3bd77685e61a7edbf
Adir Bruchim <adir.bruchim@catonetworks.com>
2025-05-09 02:11:00 +0300

    With Ball

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from classification.real_or_fake_classifier import AVClassifier
 # video_name = "GalClaps.mp4" # Real
 # video_name = "droppingDucks2.mp4" # Real
 # video_name = "clap_with_sound.mp4" # Fake
-video_name = "skateboard.mp4" # Fake
+# video_name = "skateboard.mp4" # Fake
+video_name = "FakeRedBallBouncingWithAudio.mp4" # Fake
 audio_peaks_detection(video_name)
 video_peaks_detection(video_name)
 json_path = Path(video_name).with_suffix('.json')
